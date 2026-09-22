19fec89f92632108bf55405e6b74b5356fb93fcb
HoriKana29 <arialfuaral36118@gmail.com>
2026-09-22 19:27:59 +0700

    Update README and add new assembly files for multiplier using NAND and NOR

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ProjectSCM
 
 - Website สำหรับ test Assambly https://eecs370.github.io/simulators/lc2k_v2/?utm_source=chatgpt.com 
+*** Website ดังกล่าวไม่รองรับ nand แต่รองรับ nor แทน project ใช้ nand นะ
+
 - สร้าง File Assambly สกุลไฟล์ .asm
---
+++ multiplyer_nor.asm
@@ -0,0 +1,32 @@
+        lw      0       2       mcand   # load multiplicand (3)  
+        lw      0       3       mplier  # load multiplier (4)
+        lw      0       4       mask    # load mask (1)
+        lw      0       5       count
+                                        # R6,R7 are temp registers
+
+        add     0       0       1       # initialize result = 0
+
+start   beq     0       5       done    # branch if count is 0
+
+        nor     3       3       6       # not(A) = A nor A
+        nor     4       4       7       # not(A) nor not(B) = A and B
+        nor     6       7       6       # mplier and mask, store in 6
+
+        beq     6       0       noAdd   # if the mplier and mask is 0, skip addition
+        add     1       2       1       # add multiplicand to result
+
+noAdd   add     2       2       2       # shift multiplicand left
+        add     4       4       4       # shift mask left
+
+        lw      0       6       neg1    # load -1 to temp register
+        add     5       6       5       # decrement count 
+
+        beq     0       0       start   # repeat
+
+done    halt
+                           
+mcand   .fill   3
+mplier  .fill   4
+mask    .fill   1
+count   .fill   15
+neg1    .fill   -1
